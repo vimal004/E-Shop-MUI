e395aa52a7e97e4fc91d54a3d518f7cf3db3850e
vimal004 <2004.vimal@gmail.com>
2024-07-10 19:02:31 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const App = () => {
+  return (
+    <div className="text-red-600">
+      <h1>hello</h1>
+      <Button>Hi guys</Button>
+    </div>
+  );
+};
+
+export default App;
