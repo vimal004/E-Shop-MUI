a269bacf7d626268799bb28276289a0f445a5e89
vimal004 <2004.vimal@gmail.com>
2024-07-13 16:21:27 +0530

    29

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const Card = ({ name, imageLink, rating, price, mode, qty }) => {
   return (
     <div
-      className={`w-72 h-96 rounded-lg overflow-hidden shadow-lg border border-gray-300 transform transition-transform duration-300 hover:scale-105 ${
+      className={`w-72 h-96 rounded-md overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105 ${
         mode ? "bg-gray-800" : "bg-white"
       } ${mode ? "hover:shadow-2xl" : ""} mb-6`}
     >
-      <img className="w-full h-3/5 object-cover" src={imageLink} alt={name} />
+      <img className="w-72 h-1/2 object-cover" src={imageLink} alt={name} />
       <div className={`px-6 py-4 ${mode ? "text-white" : "text-gray-800"}`}>
         <div
           className={`font-bold text-xl mb-2 ${
@@ -23,6 +23,7 @@ const Card = ({ name, imageLink, rating, price, mode, qty }) => {
         <p className={`text-base ${mode ? "text-gray-300" : "text-gray-700"}`}>
           Price: {price}
         </p>
+        <Button size="small">Add to Cart</Button>
       </div>
       <div
         className={`bg-gradient-to-t ${
