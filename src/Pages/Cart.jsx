df05b71985ed6099ec24335e745a868ab375a8a4
vimal004 <2004.vimal@gmail.com>
2024-07-14 12:00:32 +0530

    43

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -60,115 +60,131 @@ const Cart = () => {
 
   return mail.length < 5 ? (
     <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: currmode ? "#424242" : "#ffffff",
-        color: currmode ? "#ffffff" : "#000000",
-        padding: "16px",
-      }}
-    >
-      <Typography variant="h4" align="center" style={{ marginBottom: "16px" }}>
-        Sign in to add items to the cart.
-      </Typography>
-    </div>
-  ) : (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: currmode ? "#424242" : "#ffffff",
-        color: currmode ? "#ffffff" : "#000000",
-        padding: "16px",
-      }}
+      className={`min-h-screen flex flex-col ${
+        currmode ? "bg-gray-700" : "bg-white"
+      }`}
     >
       <div
         style={{
+          minHeight: "100vh",
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          marginBottom: "16px",
+          //backgroundColor: currmode ? "#424242" : "#ffffff",
+          color: currmode ? "#ffffff" : "#000000",
+          padding: "16px",
         }}
       >
         <Typography
-          variant="h3"
+          variant="h4"
+          align="center"
+          style={{ marginBottom: "16px" }}
+        >
+          Sign in to add items to the cart.
+        </Typography>
+      </div>
+    </div>
+  ) : (
+    <div
+      className={`min-h-screen flex flex-col ${
+        currmode ? "bg-gray-700" : "bg-white"
+      }`}
+    >
+      <div
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          //backgroundColor: currmode ? "#424242" : "#ffffff",
+          color: currmode ? "#ffffff" : "#000000",
+          padding: "16px",
+        }}
+      >
+        <div
           style={{
-            textAlign: "center",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
             marginBottom: "16px",
           }}
         >
-          Cart Items
-        </Typography>
-        <Box display="flex" flexDirection="column" alignItems="center">
-          <Button
-            variant="contained"
-            color="error"
-            onClick={handleDelete}
-            style={{ marginBottom: "16px", minWidth: "150px" }}
+          <Typography
+            variant="h3"
+            style={{
+              textAlign: "center",
+              marginBottom: "16px",
+            }}
           >
-            Clear Cart
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => navigate("/checkout")}
-            style={{ minWidth: "150px" }}
-          >
-            Proceed to Checkout
-          </Button>
-        </Box>
-      </div>
-
-      <Grid container spacing={3} justifyContent="center">
-        {data.length === 0 ? (
-          <Grid item xs={12}>
-            <Typography
-              variant="h5"
-              align="center"
-              style={{ marginTop: "16px" }}
+            Cart Items
+          </Typography>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <Button
+              variant="contained"
+              color="error"
+              onClick={handleDelete}
+              style={{ marginBottom: "16px", minWidth: "150px" }}
             >
-              Cart is empty
-            </Typography>
-          </Grid>
-        ) : (
-          data.map((item, index) => (
-            <Grid item key={index}>
-              <Link
-                to={`https://e-shop-gamma-sepia.vercel.app/cart/${item.product_name}`}
-              >
-                <Card
-                  name={item.product_name}
-                  rating={item.rating}
-                  price={item.price}
-                  imageLink={item.image_link}
-                  mode={currmode}
-                />
-              </Link>
-            </Grid>
-          ))
-        )}
-      </Grid>
+              Clear Cart
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => navigate("/checkout")}
+              style={{ minWidth: "150px" }}
+            >
+              Proceed to Checkout
+            </Button>
+          </Box>
+        </div>
 
-      <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={3000}
-        onClose={handleCloseSnackbar}
-        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-      >
-        <MuiAlert
-          elevation={6}
-          variant="filled"
+        <Grid container spacing={3} justifyContent="center">
+          {data.length === 0 ? (
+            <Grid item xs={12}>
+              <Typography
+                variant="h5"
+                align="center"
+                style={{ marginTop: "16px" }}
+              >
+                Cart is empty
+              </Typography>
+            </Grid>
+          ) : (
+            data.map((item, index) => (
+              <Grid item key={index}>
+                <Link
+                  to={`https://e-shop-gamma-sepia.vercel.app/cart/${item.product_name}`}
+                >
+                  <Card
+                    name={item.product_name}
+                    rating={item.rating}
+                    price={item.price}
+                    imageLink={item.image_link}
+                    mode={currmode}
+                  />
+                </Link>
+              </Grid>
+            ))
+          )}
+        </Grid>
+
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={3000}
           onClose={handleCloseSnackbar}
-          severity="success"
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
         >
-          Cart cleared successfully!
-        </MuiAlert>
-      </Snackbar>
+          <MuiAlert
+            elevation={6}
+            variant="filled"
+            onClose={handleCloseSnackbar}
+            severity="success"
+          >
+            Cart cleared successfully!
+          </MuiAlert>
+        </Snackbar>
+      </div>
     </div>
   );
 };
